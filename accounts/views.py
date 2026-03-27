83733ebbffc243421d40cb366255e29e63165eb0
Tunahan Öcal <tunahanocal@Tunahan-MacBook-Air.local>
2026-03-28 02:33:00 +0300

    11

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,8 @@
-import requests
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
+import requests
 
 def home(request):
     if request.user.is_authenticated:
@@ -53,3 +53,39 @@ def search(request):
         'books': books,
         'query': query
     })
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'accounts/register.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'accounts/login.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('home')
+
+@login_required
+def profile_view(request):
+    return render(request, 'accounts/profile.html')
+
+
