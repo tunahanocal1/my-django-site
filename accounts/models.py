0e888f173e4f979baa529bc4320ac01ea17c746d
Tunahan Öcal <tunahanocal@Tunahan-MacBook-Air.local>
2026-03-28 20:01:03 +0300

    11

--- accounts/models.py
+++ accounts/models.py
@@ -26,7 +26,7 @@ def __str__(self):
     return f"{self.user.username} Profile"
 
 class UserBook(models.Model):
-    user = models.ForeignKey(User, on_relative=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     olid = models.CharField(max_length=100)
     title = models.CharField(max_length=255)
     thumbnail = models.URLField(max_length=500, null=True, blank=True)
